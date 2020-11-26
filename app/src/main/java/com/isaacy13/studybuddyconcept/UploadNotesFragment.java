19f3560cbc382c18a93ccfabac794a70559948bc
David <fasinadavid@gmail.com>
2020-11-25 22:46:13 -0600

    Open Camera button functional

--- app/src/main/java/com/isaacy13/studybuddyconcept/UploadNotesFragment.java
+++ app/src/main/java/com/isaacy13/studybuddyconcept/UploadNotesFragment.java
@@ -1,12 +1,29 @@
 package com.isaacy13.studybuddyconcept;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.DialogInterface;
+import android.content.DialogInterface.OnClickListener;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
 
+import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
+import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+import android.widget.ImageView;
+
+import static android.app.Activity.RESULT_OK;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -23,6 +40,7 @@ public class UploadNotesFragment extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+    private Button cam;
 
     public UploadNotesFragment() {
         // Required empty public constructor
@@ -53,12 +71,29 @@ public class UploadNotesFragment extends Fragment {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
+
     }
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_upload_notes, container, false);
+
+        // Open Camera //
+        cam = view.findViewById(R.id.TakePhoto);
+        cam.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent camIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivity(camIntent);
+            }
+        });
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_upload_notes, container, false);
+        return view;
     }
+
 }
+
+
